b6cd8715b6ed6c3125fbd0b95584d96f48017890
AKasparavicius <kasparavicius.ar@gmail.com>
2023-05-09 15:07:45 +0300

    Implement CustomAdapter

---
+++ app/src/main/java/lt/arnas/androidtopics/Item.kt
@@ -0,0 +1,7 @@
+package lt.arnas.androidtopics
+
+data class Item(
+    val id: Int,
+    var text01: String,
+    var text02: String,
+)
--- app/src/main/java/lt/arnas/androidtopics/MainActivity.kt
+++ app/src/main/java/lt/arnas/androidtopics/MainActivity.kt
@@ -4,73 +4,61 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityLifeCycles() {
 
     lateinit var firstButton: Button
+    lateinit var adapter: CustomAdapter
+    lateinit var itemListView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         firstButton = findViewById(R.id.firstButton)
-
-        timber("onCreate")
+        itemListView = findViewById(R.id.itemListView)
 
         openSecondActivity()
-    }
 
-    override fun onStart() {
-        super.onStart()
-        timber("onStart")
-    }
+        val items = mutableListOf<Item>()
+        generateListOfItems(items)
 
-    override fun onRestart() {
-        super.onRestart()
-        timber("onRestart")
-    }
+        for (item in items){
+            Timber.i(item.toString())
+        }
 
-    override fun onResume() {
-        super.onResume()
-        timber("onResume")
-    }
-
-    override fun onStop(){
-        super.onStop()
-        timber("onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        timber("onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timber("onDestroy")
-    }
-
-//    override fun onRestart(){
-//        super.onRestart()
-//        timber("onRestart")
-//    }
-
-    fun timber(text: String){
-        Timber.i(
-            """
-                ***************************
-                * ${this.localClassName}
-                * $text
-                ***************************
-            """.trimIndent()
+        adapter = CustomAdapter(this)
+        adapter.add(items)
+        adapter.add(Item(101, "text01", "text02"))
+        adapter.add(
+            Item(102, "text01", "text02"),
+            Item(103, "text01", "text02"),
+            Item(104, "text01", "text02"),
+            Item(105, "text01", "text02"),
         )
+        itemListView.adapter = adapter
+
     }
+
 
     private fun openSecondActivity(){
         firstButton.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun generateListOfItems(items: MutableList<Item>) {
+        for (item in 1..10){
+            items.add(
+                Item(
+                    item,
+                    "Text01_%04x".format(item),
+                    "Text02_%06x".format(item))
+            )
         }
     }
 }
