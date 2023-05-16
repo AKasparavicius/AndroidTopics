6411e2b1971252632a312d29c69db23d4dd403d3
AKasparavicius <kasparavicius.ar@gmail.com>
2023-05-16 15:57:53 +0300

    Implement ViewBinding in MainActivity

--- app/src/main/java/lt/arnas/androidtopics/MainActivity.kt
+++ app/src/main/java/lt/arnas/androidtopics/MainActivity.kt
@@ -9,22 +9,27 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import androidx.activity.result.contract.ActivityResultContracts
+import lt.arnas.androidtopics.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : ActivityLifeCycles() {
 
-    lateinit var firstButton: Button
-    lateinit var adapter: CustomAdapter
-    lateinit var itemListView: ListView
+//    private lateinit var firstButton: Button
+    private lateinit var adapter: CustomAdapter
+//    private lateinit var itemListView: ListView
+    private var itemIndex = -1
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        firstButton = findViewById(R.id.firstButton)
-        itemListView = findViewById(R.id.itemListView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        openSecondActivity()
+
+//        firstButton = findViewById(R.id.firstButton)
+//        itemListView = findViewById(R.id.itemListView)
 
         val items = mutableListOf<Item>()
         generateListOfItems(items)
@@ -33,8 +38,26 @@ class MainActivity : ActivityLifeCycles() {
             Timber.i(item.toString())
         }
 
+//        adapter = CustomAdapter(this)
+//        adapter.add(items)
+//        adapter.add(Item(101, "text01", "text02"))
+//        adapter.add(
+//            Item(102, "text01", "text02"),
+//            Item(103, "text01", "text02"),
+//            Item(104, "text01", "text02"),
+//            Item(105, "text01", "text02"),
+//        )
+//        itemListView.adapter = adapter
+        setUpListView()
+        openSecondActivity()
+        setClickOpenItemDetails()
+
+    }
+
+    private fun setUpListView() {
         adapter = CustomAdapter(this)
-        adapter.add(items)
+        binding.itemListView.adapter = adapter
+
         adapter.add(Item(101, "text01", "text02"))
         adapter.add(
             Item(102, "text01", "text02"),
@@ -42,16 +65,11 @@ class MainActivity : ActivityLifeCycles() {
             Item(104, "text01", "text02"),
             Item(105, "text01", "text02"),
         )
-        itemListView.adapter = adapter
-
-        setClickOpenItemDetails()
-
     }
 
 
-
     private fun openSecondActivity(){
-        firstButton.setOnClickListener {
+        binding.firstButton.setOnClickListener {
 //            val intent = Intent(this, SecondActivity::class.java)
 //            startActivity(intent)
             startActivityForResult.launch(Intent(this, SecondActivity::class.java))
@@ -59,14 +77,17 @@ class MainActivity : ActivityLifeCycles() {
     }
 
     private fun setClickOpenItemDetails() {
-        itemListView.setOnItemClickListener { adapterView, view, position, l ->
+        binding.itemListView.setOnItemClickListener { adapterView, view, position, l ->
             val item: Item = adapterView.getItemAtPosition(position) as Item
+            itemIndex = position
 
             val itemIntent = Intent(this, SecondActivity::class.java)
             itemIntent.putExtra(MAIN_ACTIVITY_KEY_ITEM_ID, item.id)
             itemIntent.putExtra(MAIN_ACTIVITY_KEY_ITEM_TEXT01, item.text01)
             itemIntent.putExtra(MAIN_ACTIVITY_KEY_ITEM_TEXT02, item.text02)
-            startActivity(itemIntent)
+
+//            startActivity(itemIntent)
+            startActivityForResult.launch(itemIntent)
         }
     }
 
@@ -85,7 +106,17 @@ class MainActivity : ActivityLifeCycles() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             result ->
             when(result.resultCode){
-                Activity.RESULT_OK -> {
+                SecondActivity.SECOND_ACTIVITY_ITEM_INTENT_RETURN_NEW -> {
+                    val item = Item(
+                        id = 111,
+                        text01 = result.data
+                            ?.getStringExtra(SecondActivity.SECOND_ACTIVITY_KEY_ITEM_TEXT01) ?: "",
+                        text02 = result.data
+                            ?.getStringExtra(SecondActivity.SECOND_ACTIVITY_KEY_ITEM_TEXT02) ?: ""
+                    )
+                    adapter.add(item)
+                }
+                SecondActivity.SECOND_ACTIVITY_ITEM_INTENT_RETURN_UPDATE -> {
                     val item = Item(
                         id = result.data
                             ?.getIntExtra(SecondActivity.SECOND_ACTIVITY_KEY_ITEM_ID, 0) ?: 0,
@@ -94,8 +125,9 @@ class MainActivity : ActivityLifeCycles() {
                         text02 = result.data
                             ?.getStringExtra(SecondActivity.SECOND_ACTIVITY_KEY_ITEM_TEXT02) ?: ""
                     )
-
-                    adapter.add(item)
+                    if(itemIndex >= 0) {
+                        adapter.update(itemIndex, item)
+                    }
                 }
             }
         }
